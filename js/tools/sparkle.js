71193be53cb9b039b0db8cc16b2fe367475375c0
Shayna Kaushal <shaynakaushal@gmail.com>
2022-10-12 10:53:28 -0500

    pink 5pt stars

--- js/tools/sparkle.js
+++ js/tools/sparkle.js
@@ -17,41 +17,26 @@ window.tools.sparkle = {
       // if self tool is selected AND the mouse is pressed
       if (self.state.selected && self.state.mousePressed) {
         const mouse = app.eventToMouse(e)
-        // const px = self.state.prevMouse.x || mouse.x
-        // const py = self.state.prevMouse.y || mouse.y
         const alpha = (2 * Math.PI) / 10
-        const r = 5
+        // const r = 5
+        var r = Math.abs(Math.sin(Date.now() / 500) * 10)
         var star_pt = [mouse.x, mouse.y]
 
         app.ctx.beginPath()
 
-        for (var i = 11; i != 0; i--) {
+        for (var i = 10; i > 0; i--) {
           var r_tmp = r * (i % 2 + 1)
           var omega = alpha * i
           app.ctx.lineTo((r_tmp * Math.sin(omega)) + star_pt[0], (r_tmp * Math.cos(omega)) + star_pt[1])
         }
 
-        // app.ctx.moveTo(mouse.x, mouse.y)
-        // app.ctx.lineTo(px, py)
         app.ctx.closePath()
-        // app.ctx.fillStyle = "#000"
         app.ctx.fill()
-        // app.ctx.stroke()
-        // update prevMouse coordinates
-        self.state.prevMouse = { x: mouse.x, y: mouse.y }
+        app.ctx.fillStyle = 'pink'
+        app.ctx.strokeStyle = 'blue'
+        app.ctx.stroke()
+        // self.state.prevMouse = { x: mouse.x, y: mouse.y }
       }
-
-      // if (self.state.selected && self.state.mousePressed) {
-      //   const mouse = app.eventToMouse(e)
-      //   const radius = Math.abs(Math.sin(Date.now() / 500) * 50)
-      //   app.ctx.beginPath()
-      //   app.ctx.arc(mouse.x, mouse.y, radius, 0, 2 * Math.PI, false)
-      //   app.ctx.fill()
-      //   app.ctx.fillStyle = 'white'
-      //   // app.ctx.lineWidth = 5
-      //   app.ctx.stroke()
-      // }
-
     }
   }
 }
